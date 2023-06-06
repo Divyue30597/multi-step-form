382a14d2298e6ecfa6f3ca9709018752addf19a4
Divyue30597 <tomchi.sharma305@outlook.com>
2023-06-06 23:15:34 +0530

    working on toggle

--- src/Component/Card/Card.tsx
+++ src/Component/Card/Card.tsx
@@ -7,23 +7,94 @@ interface ICardDetail {
   type: string;
   price: number;
   plan: string;
+  alt: string;
 }
 
 const cardDetails: ICardDetail[] = [
-  { cardIcon: iconAdvance, type: "Advance", price: 12, plan: "monthly" },
-  { cardIcon: iconAdvance, type: "Advance", price: 120, plan: "yearly" },
-  { cardIcon: iconPro, type: "Pro", price: 15, plan: "monthly" },
-  { cardIcon: iconPro, type: "Pro", price: 150, plan: "yearly" },
-  { cardIcon: iconArcade, type: "Arcade", price: 9, plan: "monthly" },
-  { cardIcon: iconArcade, type: "Arcade", price: 90, plan: "yearly" },
+  {
+    cardIcon: iconAdvance,
+    type: "Advance",
+    price: 12,
+    plan: "monthly",
+    alt: "red background with game pad.",
+  },
+  {
+    cardIcon: iconAdvance,
+    type: "Advance",
+    price: 120,
+    plan: "yearly",
+    alt: "red background with game pad.",
+  },
+  {
+    cardIcon: iconPro,
+    type: "Pro",
+    price: 15,
+    plan: "monthly",
+    alt: "blue background with controller.",
+  },
+  {
+    cardIcon: iconPro,
+    type: "Pro",
+    price: 150,
+    plan: "yearly",
+    alt: "blue background with controller.",
+  },
+  {
+    cardIcon: iconArcade,
+    type: "Arcade",
+    price: 9,
+    plan: "monthly",
+    alt: "Orange background with atari type console",
+  },
+  {
+    cardIcon: iconArcade,
+    type: "Arcade",
+    price: 90,
+    plan: "yearly",
+    alt: "Orange background with atari type console",
+  },
 ];
 
-export function Card() {
+export function Card({ isMonthly }: { isMonthly: boolean }) {
   return (
     <div className="cards">
       {cardDetails.map((cardDetail: ICardDetail) => {
-        return <div key={cardDetail.price}>CardBody</div>;
+        {
+          return isMonthly && cardDetail.plan === "monthly" ? (
+            <CardMonthlyStyle key={cardDetail.price} cardDetail={cardDetail} />
+          ) : !isMonthly && cardDetail.plan === "yearly" ? (
+            <CardYearlyStyle key={cardDetail.price} cardDetail={cardDetail} />
+          ) : null;
+        }
       })}
+    </div>
+  );
+}
+
+function CardMonthlyStyle({ cardDetail }: { cardDetail: ICardDetail }) {
+  return (
+    <div className="card-body">
+      <picture>
+        <img src={cardDetail.cardIcon} alt={cardDetail.alt} />
+      </picture>
+      <div className="card-body-details">
+        <h3>{cardDetail.type}</h3>
+        <p>${cardDetail.price}/mo</p>
+      </div>
+    </div>
+  );
+}
+
+function CardYearlyStyle({ cardDetail }: { cardDetail: ICardDetail }) {
+  return (
+    <div className="card-body">
+      <picture>
+        <img src={cardDetail.cardIcon} alt={cardDetail.alt} />
+      </picture>
+      <div className="card-body-details">
+        <h3>{cardDetail.type}</h3>
+        <p>${cardDetail.price}/yr</p>
+      </div>
     </div>
   );
 }
--- src/Component/FormPage/SelectPlan.tsx
+++ src/Component/FormPage/SelectPlan.tsx
@@ -1,7 +1,16 @@
+import { useState } from "react";
+import { Card } from "../Card/Card";
+
 export function SelectPlan() {
+  const [isMonthly, setIsMonthly] = useState<boolean>(true);
+
   return (
-    <div>
-      <h1>Select plan</h1>
+    <div className="select-plan">
+      <Card isMonthly={isMonthly} />
+      <div className="select-plan-toggle">
+        <span>Monthly</span>
+        <span>Yearly</span>
+      </div>
     </div>
   );
 }
